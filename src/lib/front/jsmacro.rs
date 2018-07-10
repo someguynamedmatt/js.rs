a2ae7a0175739a026ed499288e44057e3d46730e
Matt <young.qubit@gmail.com>
2018-07-09 20:28:07 -0700

    macro rewrite: better pattern matching

--- src/lib/front/jsmacro.rs
+++ src/lib/front/jsmacro.rs
@@ -1,10 +1,19 @@
 #[macro_export]
 macro_rules! js(
-	($global:expr, { $( $name:tt: $value:tt ),* }) => ({
+	($global:expr, {
+        $( $name:tt: $value:expr ),+
+    }) => ({
 		let value = Value::new_obj(Some($global));
 		$(
 			value.set_field($name, js!($value));
-		)+
+		)*
+		value
+	});
+	($global:expr, {
+        $name:tt: $value:expr
+    }) => ({
+		let value = Value::new_obj(Some($global));
+        value.set_field($name, js!($value));
 		value
 	});
 	($inp:expr) => (
@@ -13,15 +22,15 @@ macro_rules! js(
 );
 #[macro_export]
 macro_rules! js_extend(
-	($object:expr, { $name:tt: $value:tt }) => (
+	($object:expr, { $name:tt: $value:expr }) => (
 		$object.set_field($name, $value)
 	);
 	($object:expr, {
-		$($name:tt, $value:tt),+
+		$($name:tt: $value:expr),+
 	}) => ({
 		let object = $object;
 		$(
 			object.set_field($name, js!($value));
-		)+
+		)*
 	});
 );
